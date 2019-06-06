12cb27a012546e1ffbd5197d01b8e6334dd56d99
Giorgos <skilopsaros@gmail.com>
2019-06-06 15:36:51 +0100

    Creatures and food now generated in Environment
    Environment file now has functions to generate variables for creatures and food, while the Environment object only needs the number of each to generate them itself. Everything in UI transfered to Environment file

--- Environment.py
+++ Environment.py
@@ -1,4 +1,61 @@
 #File to add code for the environment in which the creatures reside
+import Creatures as crt
+import numpy as np
+
+def generateRandomNumberWithLowerLimit (mean, sigma, lower_limit):
+    x = np.random.normal(mean, sigma)
+    while (x < lower_limit):
+        x = np.random.normal(mean, sigma)
+    return x
+
+def generateFoodVariables (length_of_map, mean_energy = 1.0):
+    '''
+    Takes the length of the map from side to side and generates a random position, with higher chance of it being near the centre
+    '''
+
+    x = np.random.normal(0.0, 15)
+    while (abs(x) > length_of_map/2):
+        x = np.random.normal(0.0, 15)
+    y = np.random.normal(0.0, 15)
+    while (abs(y) > length_of_map/2):
+        y = np.random.normal(0.0, 15)
+    energy = generateRandomNumberWithLowerLimit (mean_energy, 0.2, 0.01)
+
+    return x,y,energy
+
+def generateCreatureVariables (length_of_map, mean_speed = 1.0, mean_radius = 1.5, mean_sensory_range = 40):
+    '''
+    Takes the length of the map from side to side and generates all the random variables needed for a creature to work. the result cannot be passed directly to the creature init function since it lacks environment
+    '''
+    #chooses a random part of the borders to spawn the creatures in
+    border = np.random.choice([1, 2, 3, 4])  
+    if (1 == border):
+        y = -50
+        x = np.random.normal(0.0, 15)
+        while (abs(x) > length_of_map/2):
+            x = np.random.normal(0.0, 15)
+    elif (2 == border):
+        x = -50
+        y = np.random.normal(0.0, 15)
+        while (abs(y) > length_of_map/2):
+            y = np.random.normal(0.0, 15)
+    elif (3 == border):
+        y = 50
+        x = np.random.normal(0.0, 15)
+        while (abs(x) > length_of_map/2):
+            x = np.random.normal(0.0, 15)
+    elif (4 == border):
+        x = 50
+        y = np.random.normal(0.0, 15)
+        while (abs(y) > length_of_map/2):
+            y = np.random.normal(0.0, 15)
+
+
+    speed         = generateRandomNumberWithLowerLimit (mean_speed, 0.3, 0.1)
+    radius        = generateRandomNumberWithLowerLimit (mean_radius, 0.2, 0.1)
+    sensory_range = generateRandomNumberWithLowerLimit (mean_sensory_range, 5, 0.1)
+    return  x, y, speed, radius, sensory_range
+
 def getDistance(position_one, position_two):
     '''
     Takes in two touples of (x,y) locations and finds the distance between them
@@ -7,12 +64,16 @@ def getDistance(position_one, position_two):
 
 
 class Environment(object):
-    def __init__(self, creatures, foods, tick_length = 1):
+    def __init__(self, number_of_creatures, number_of_foods, tick_length = 1):
 
         '''
         Defines the enviroment on the class calling
         '''
 
+        creatures = []
+        for i in range (number_of_creatures):
+            x, y, speed, radius, sensory_range = generateCreatureVariables (100)
+            creaturs.append(crt.Creature(x, y, speed, 250, radius, sensory_range, self))
         self.creatures = creatures
         #will get the position of each creature
         positions_of_creatures = []
@@ -20,6 +81,9 @@ class Environment(object):
             positions_of_creatures.append(creatures[i].getLocation())
         self.positions_of_creatures = positions_of_creatures
 
+        foods = []
+        for i in range (number_of_foods):
+            foods.append(Food(generateFoodVariables (100)))
         self.foods = foods
         #will get the position of each piece of food
         for i in range (len(foods)):
@@ -81,10 +145,10 @@ class Food(object):
         Defines a piece of food on the class calling
         '''
 
-        self.energy = energy
+        self.energy     = energy
         self.x_location = x_location
         self.y_location = y_location
-        self.radius = pow((energy/420000),(1/3))
+        self.radius     = pow((energy/420000),(1/3))
 
 
     def getLocation(self):
--- UI.py
+++ UI.py
@@ -2,25 +2,6 @@
 import Environment as env
 import Creatures as crt
 import matplotlib.pyplot as plt
-import numpy as np
-
-def generateFoodVariables (length):
-    '''
-    Takes the length of the map from side to side and generates a random position, with higher chance of it being near the centre
-    '''
-    x = np.random.normal(0.0, 15)
-    while (abs(x) > length/2):
-        x = np.random.normal(0.0, 15)
-    y = np.random.normal(0.0, 15)
-    while (abs(y) > length/2):
-        y = np.random.normal(0.0, 15)
-    energy = np.random.normal(1.0, 0.2)
-    while (energy < 0):
-        energy = np.random.normal(0.0, 15)
-    return x,y,energy
 
 #for the test, let's create 6 pieces of food and one creature
-foods = []
-for i in range (6):
-    foods.append(env.Food(generateFoodVariables (100)))
-creatures = [crt.Creature()]#this needs to be filled in
+
